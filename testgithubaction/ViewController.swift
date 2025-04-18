348b773e17692d5bdd96d4af005b2689cc8e879e
dt_8705858457 <kiss.loo@qq.com>
2025-04-18 15:03:13 +0800

    打包测试

--- testgithubaction/ViewController.swift
+++ testgithubaction/ViewController.swift
@@ -6,26 +6,26 @@
 //
 
 import UIKit
-import GoogleSignIn
+import Alamofire
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
-            if let error = error {
-                // 处理错误
-            } else if let signInResult = signInResult {
-                // 用户成功登录
-                signInResult.user.refreshTokensIfNeeded { user, error in
-                    guard error == nil else { return }
-                    guard let user = user else { return }
-                    
-         
-        
-                }
-            }
-        }
+//        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+//            if let error = error {
+//                // 处理错误
+//            } else if let signInResult = signInResult {
+//                // 用户成功登录
+//                signInResult.user.refreshTokensIfNeeded { user, error in
+//                    guard error == nil else { return }
+//                    guard let user = user else { return }
+//                    
+//         
+//        
+//                }
+//            }
+//        }
     }
 
 
